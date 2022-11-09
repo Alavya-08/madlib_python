28aa433a14817d6fbdea20b014f0ef6a190b28a6
Alavya <83856017+Alavya-08@users.noreply.github.com>
2022-11-09 12:03:35 +0530

    main file

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+# basic project ot understand the string concatenation  using mad lib
+
+
+adj = input("Adjective:")
+verb1 = input("Verb:")
+verb2 = input("Verb:")
+famous_name = input("Famous Name:")
+
+
+madlib = f"Life is so {adj}! It makes me so excited all the time because\nI love to {verb1}. Stay hydrated and {verb2} like you are {famous_name}!"
+
+# to get the output
+print(madlib)
